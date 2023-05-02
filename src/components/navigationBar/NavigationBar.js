a2cc1042e494598e45e97374a9e318246d0d750c
lexdolfing <lexdolfing@gmail.com>
2023-05-02 16:02:34 +0200

    added mobile menu to navigationbar

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './NavigationBar.module.css';
 import { NavLink } from "react-router-dom";
-import sliders from '../../assets/slider-logo.jpg'
 import Logo from "../logo/Logo";
 
+
 export default function NavigationBar(){
+
+  const [displayMenu, toggleDisplayMenu] = useState(false);
 
     return(
         <nav className={styles['navigation_bar']}>
@@ -13,6 +15,17 @@ export default function NavigationBar(){
             <Logo />
             <NavLink to="/sign-in" className={({ isActive }) => isActive ? styles['active-link'] : styles['default-link']}>Login</NavLink>
             <NavLink to="/sign-up" className={({ isActive }) => isActive ? styles['active-link'] : styles['default-link']}>Register</NavLink>
+            <button className={styles['menu-toggle']} onClick={ () => toggleDisplayMenu(!displayMenu)}>
+                <span className={styles.hamburger}></span>
+                {displayMenu &&
+                    <div className={styles['mobile-menu']}>
+                    <NavLink to="/" className={({ isActive }) => isActive ? styles['active-mobile-link'] : styles['default-mobile-link']}>Home</NavLink>
+                    <NavLink to="/drop-your-demo" className={({ isActive }) => isActive ? styles['active-mobile-link'] : styles['default-mobile-link']}>Demo Drop</NavLink>
+                    <NavLink to="/sign-in" className={({ isActive }) => isActive ? styles['active-mobile-link'] : styles['default-mobile-link']}>Login</NavLink>
+                    <NavLink to="/sign-up" className={({ isActive }) => isActive ? styles['active-mobile-link'] : styles['default-mobile-link']}>Register</NavLink>
+                    </div>
+                }
+            </button>
         </nav>
     )
 }
