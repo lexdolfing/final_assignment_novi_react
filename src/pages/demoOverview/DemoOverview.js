d8b3462666f2355043651db6c035908c18ca0f49
lexdolfing <lexdolfing@gmail.com>
2023-05-03 15:59:04 +0200

    added get-request and injected data in web page with map function

--- src/pages/demoOverview/DemoOverview.js
+++ src/pages/demoOverview/DemoOverview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import stylesIndex from '../../index.module.css';
 import styles from './DemoOverview.module.css';
 import NavigationBar from "../../components/navigationBar/NavigationBar";
@@ -7,14 +7,38 @@ import Button from "../../components/button/Button";
 import {useNavigate} from "react-router-dom";
 import ReactAudioPlayer from "react-audio-player";
 import sampleMp3 from '../../assets/sample-3s.mp3'
+import axios from "axios";
 
 
 export default function DemoOverview() {
     const navigate = useNavigate();
+    const [demodata, setDemoData] = useState([])
+    const [mp3Selected, setMp3Selected] = useState(sampleMp3)
 
-    function replyToDemo() {
-        // navigate('/reply-to-demo')
+    function replyToDemo(demoId) {
+        // Hier moet link naar pagina gaan met dynamische link naar reply-to-demo/{demoId}
     }
+
+    function playSong(mp3File) {
+        setMp3Selected(mp3File);
+        console.log(mp3File);
+    }
+
+    useEffect(() => {
+
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://localhost:8081/demos');
+                console.log(response);
+                setDemoData(response.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        void fetchData()
+
+    }, [])
 
     return (
         <>
@@ -26,49 +50,28 @@ export default function DemoOverview() {
 
                     <table className={styles.table}>
                         <thead>
-                        <tr  className={styles['table-header']}>
+                        <tr className={styles['table-header']}>
                             <th>Reply</th>
                             <th>#</th>
                             <th>artist</th>
                             <th>title</th>
-                            <th>dropped by</th>
+                            <th>song inspiration</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {/*//map for songs, now hardcoded one*/}
-                        <tr onClick={replyToDemo} className={styles['demo-row']}>
-                            <th><Button bigOrSmall="super-small-button" onClick={replyToDemo} button_content="Reply"
-                                        buttonType="button"/></th>
-                            <td>1</td>
-                            <td>Lorem Ipsum</td>
-                            <td>Dolor sit amet consectetur</td>
-                            <td>Lorem Ipsum</td>
-                        </tr>
-                        <tr>
-                            <th><Button bigOrSmall="super-small-button" onClick={replyToDemo} button_content="Reply"
-                                        buttonType="button"/></th>
-                            <td>1</td>
-                            <td>Lorem Ipsum</td>
-                            <td>Dolor sit amet consectetur</td>
-                            <td>Lorem Ipsum</td>
-                        </tr>
-                        <tr>
-                            <th><Button bigOrSmall="super-small-button" onClick={replyToDemo} button_content="Reply"
-                                        buttonType="button"/></th>
-                            <td>1</td>
-                            <td>Lorem Ipsum</td>
-                            <td>Dolor sit amet consectetur</td>
-                            <td>Lorem Ipsum</td>
-                        </tr>
-                        <tr>
-                            <th><Button bigOrSmall="super-small-button" onClick={replyToDemo} button_content="Reply"
-                                        buttonType="button"/></th>
-                            <td>1</td>
-                            <td>Lorem Ipsum</td>
-                            <td>Dolor sit amet consectetur</td>
-                            <td>Lorem Ipsum</td>
-                        </tr>
-
+                        {demodata.map((demo) => {
+                            return (
+                                <tr key={demo.id} className={styles['demo-row']} onClick={() => playSong(demo.mp3File)}>
+                                    <th><Button bigOrSmall="super-small-button" onClick={() => replyToDemo(demo.id)}
+                                                button_content="Reply"
+                                                buttonType="button"/></th>
+                                    <td>{demo.id}</td>
+                                    <td>{demo.artistName}</td>
+                                    <td>{demo.songName}</td>
+                                    <td>{demo.songElaboration}</td>
+                                </tr>
+                            )
+                        })}
                         </tbody>
 
                     </table>
@@ -78,11 +81,11 @@ export default function DemoOverview() {
             <section className={stylesIndex['outer-container']}>
                 <section className={stylesIndex['inner-container']}>
 
-            <ReactAudioPlayer
-                src={sampleMp3}
-                autoPlay={false}
-                controls={true}
-            />
+                    <ReactAudioPlayer
+                        src={mp3Selected}
+                        autoPlay={false}
+                        controls={true}
+                    />
                 </section>
             </section>
             <Footer/>
