dccdd3cf1781c5fcc1fd0cbfa507f5ef2143b867
lexdolfing <lexdolfing@gmail.com>
2023-05-10 15:56:28 +0200

    added useEffect with two get requests in ViewReply

--- src/pages/viewReply/ViewReply.js
+++ src/pages/viewReply/ViewReply.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import styles from './ViewReply.module.css'
+import {useParams} from "react-router-dom";
 import stylesIndex from "../../index.module.css";
 import NavigationBar from "../../components/navigationBar/NavigationBar";
 
 export default function ViewReply() {
     const {id} = useParams();
     const [replyData, setReplyData] = useState([]);
+    const [demoId, setDemoId] = useState();
+    const [demoData, setDemoData] = useState([]);
 
 
     useEffect(() => {
@@ -15,22 +18,41 @@ export default function ViewReply() {
                     const response = await axios.get(`http://localhost:8081/reply-to-demo/${id}`);
                     console.log(response);
                     setReplyData(response.data);
+                    setDemoId(replyData.demoID);
                 } catch (e) {
                     console.error(e);
                 }
+            }
 
+            async function fetchDemoData() {
+                try {
+                    const response = await axios.get(`http://localhost:8081/demos/${demoId}`);
+                    console.log(response);
+                    setDemoData(response.data);
+                } catch (e) {
+                    console.error(e);
+                }
             }
 
             void fetchData();
+            void fetchDemoData();
         }, []
     )
 
     return (
         <body className={stylesIndex['page-body']}>
-        <NavigationBar />
-        <h1>View reply to demo page</h1>
-        <p>Het Id nummer van de reply is {id}</p>
-
+        <NavigationBar/>
+        <section className={stylesIndex['outer-container']}>
+            <section className={stylesIndex['inner-container']}>
+                <article className={stylesIndex['form-container']}>
+                    <h1>This is the admin reply to demo with title {demoData.songName}</h1>
+                    <h2>Decision of the Talent Manager</h2>
+                    <p>{replyData.adminDecision}</p>
+                    <h2>Comments from the Talent Manager on the demo</h2>
+                    <p>{replyData.adminComments}</p>
+                </article>
+            </section>
+        </section>
         </body>
 
     )
